61591ad8f837517c1cd904abfb7c1ac83d5167a2
micuat <micuat@gmail.com>
2019-10-04 15:33:19 +0800

    preset duration for transition

--- openposeVis/data/sketch.js
+++ openposeVis/data/sketch.js
@@ -5,10 +5,10 @@ ws.onopen = function (event) {
 		radios[i].onclick = function () {
 			const preset = presets[this.value];
 			for (let j = 0; j < numSliders; j++) {
-				TweenLite.to(document.getElementById('slide' + j), 3, { value: preset[j], autoAlpha: 0, ease: Power2.easeInOut });
+				TweenLite.to(document.getElementById('slide' + j), preset.duration, { value: preset.sliders[j], autoAlpha: 0, ease: Power2.easeInOut });
 			}
 			let count = 0;
-			let maxCount = 300;
+			let maxCount = preset.duration * 1000 / 10;
 			let interval = setInterval(function () {
 				for (let j = 0; j < numSliders; j++) {
 					sliderValues[j] = document.getElementById('slide' + j).value;
@@ -41,14 +41,14 @@ ws.onopen = function (event) {
 const numSliders = 10;
 const sliderValues = new Array(numSliders);
 const presets = {
-	grid: [0, 0, 0, 0, 1000, 0, 0, 0, 200, 0],
-	pfOnGrid: [0, 0, 0, 0, 1000, 0, 0, 0, 200, 1000],
-	wandering: [200, 0, 0, 0, 1000, 1000, 0, 1000, 1000, 0],
-	armOnBlack: [400, 0, 0, 0, 1000, 0, 0, 1000, 0, 0],
-	armOnVideo: [400, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-	armAndLeg: [400, 400, 0, 0, 0, 0, 0, 0, 0, 0],
-	all: [400, 400, 400, 0, 0, 0, 0, 0, 0, 0],
-	keith: [0, 200, 300, 0, 1000, 0, 0, 0, 0, 0]
+	grid: { sliders: [0, 0, 0, 0, 1000, 0, 0, 0, 200, 0], duration: 1},
+	pfOnGrid: { sliders: [0, 0, 0, 0, 1000, 0, 0, 0, 200, 1000], duration: 1},
+	wandering: { sliders: [200, 0, 0, 0, 1000, 1000, 0, 1000, 1000, 0], duration: 1},
+	armOnBlack: { sliders: [400, 0, 0, 0, 1000, 0, 0, 1000, 0, 0], duration: 3},
+	armOnVideo: { sliders: [400, 0, 0, 0, 0, 0, 0, 0, 0, 0], duration: 8},
+	armAndLeg: { sliders: [400, 400, 0, 0, 0, 0, 0, 0, 0, 0], duration: 1},
+	all: { sliders: [400, 400, 400, 0, 0, 0, 0, 0, 0, 0], duration: 1},
+	keith: { sliders: [0, 200, 300, 0, 1000, 0, 0, 0, 0, 0], duration: 1}
 };
 
 let radios = document.forms['presetForm'].elements['preset'];
